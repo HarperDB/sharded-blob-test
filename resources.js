9e44e3cebe3b276b6c3a7afffe68eea61156d71f
Kyle Bernhardy <kyle@harperdb.io>
2025-03-06 16:29:54 -0700

    Initial commit

---
+++ resources.js
@@ -0,0 +1,9 @@
+const {Shardnado} = databases.shard;
+
+//sample id: itemId=10052100863&deviceType=desktop&upstream=www.walmart.com
+Shardnado.setResidencyById((id ) => {
+  let matchId = id.match(/itemId=([\d.]+)/);
+  const lastDigit = (matchId[1] % 10) +1;
+  //return [`lax${lastDigit}`, `ord${lastDigit}`]
+  return matchId[1] % 2 === 0 ? ['shard1'] : ['shard2'];
+});
