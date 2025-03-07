36dbdc185546d53ce7a7851a286c199eff74eea3
Kyle Bernhardy <kyle@harperdb.io>
2025-03-07 15:01:11 -0700

    added setResidency

--- resources.js
+++ resources.js
@@ -1,9 +1,19 @@
 const {Shardnado} = databases.shard;
 
 //sample id: itemId=10052100863&deviceType=desktop&upstream=www.walmart.com
-Shardnado.setResidencyById((id ) => {
+/*Shardnado.setResidencyById((id ) => {
   let matchId = id.match(/itemId=([\d.]+)/);
   const lastDigit = (matchId[1] % 10) +1;
   //return [`lax${lastDigit}`, `ord${lastDigit}`]
+  console.log(matchId[1] % 2 === 0 ? ['shard1'] : ['shard2'])
+  return matchId[1] % 2 === 0 ? ['shard1'] : ['shard2'];
+});*/
+
+Shardnado.setResidency((record ) => {
+  const id = record.cacheKey;
+  let matchId = id.match(/itemId=([\d.]+)/);
+  const lastDigit = (matchId[1] % 10) +1;
+  //return [`lax${lastDigit}`, `ord${lastDigit}`]
+  console.log(matchId[1] % 2 === 0 ? ['shard1'] : ['shard2'])
   return matchId[1] % 2 === 0 ? ['shard1'] : ['shard2'];
 });
